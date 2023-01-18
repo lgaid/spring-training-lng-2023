a835da1f8faefe8e28ea87f7fc8e31ee2f5d6c51
lgaid <104643045+lgaid@users.noreply.github.com>
2023-01-17 22:03:59 -0500

    Initial commit

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/Spring05CoreAbstractionApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring05CoreAbstractionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring05CoreAbstractionApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cydeo/SpringTrainingLng2023Application.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainingLng2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrainingLng2023Application.class, args);
+    }
+
+}
